4471df65562a8f37d638c502b75771b41cb9cab4
Jon Hermansen <jon@jh86.org>
2025-08-28 00:40:23 -0400

    More testing

--- flake.nix
+++ flake.nix
@@ -1,22 +1,25 @@
 {
-  inputs.nixos-generators = {
-    url = "github:nix-community/nixos-generators";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, nixos-generators, ... }:
+  outputs = { nixpkgs, nixos-generators, ... }: 
     let
-      formats = ["amazon" "azure" "cloudstack" "do" "docker" "gce" "hyperv" "install-iso" "install-iso-hyperv" "iso" "kexec" "kexec-bundle" "kubevirt" "linode" "lxc" "lxc-metadata" "openstack" "proxmox" "proxmox-lxc" "qcow" "qcow-efi" "raw" "raw-efi" "sd-aarch64" "sd-aarch64-installer" "sd-x86_64" "vagrant-virtualbox" "virtualbox" "vm" "vm-bootloader" "vm-nogui" "vmware"];
-      systems = ["x86_64-linux" "aarch64-linux"];
-      mkFormat = format: system: nixos-generators.nixosGenerate {
-        inherit system format;
-        modules = [];
+      systems = ["x86_64-linux" "aarch64-linux" "riscv64-linux"];
+      mkMachine = system: nixpkgs.lib.nixosSystem {
+        modules = [
+          nixos-generators.nixosModules.all-formats
+          { nixpkgs.hostPlatform = system; }
+        ];
       };
-      mkFormats = builtins.listToAttrs (builtins.concatMap (format:
-        map (system: {
-          name = "${format}-${system}";
-          value = mkFormat format system;
-        }) systems
-      ) formats);
-    in mkFormats;
+    in {
+      nixosConfigurations = builtins.listToAttrs (map (system: {
+        name = "machine-${system}";
+        value = mkMachine system;
+      }) systems);
+    };
 }
